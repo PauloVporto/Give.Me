1e759b4f26c194ae1310012c95ed186a69871557
Dimitri <dimitri.s.amado@gmail.com>
2025-11-14 10:51:48 -0300

    fix: Removendo comentários desnecessários e corrigindo identação de código (lint, black)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,23 +8,23 @@ from .models import Category, City, Item, ItemPhoto, Notification, UserProfile
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug']
+        fields = ["id", "name", "slug"]
 
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ['id', 'name', 'state']
+        fields = ["id", "name", "state"]
 
 
 class ItemPhotoSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = ItemPhoto
-        fields = ['id', 'image', 'url', 'position', 'created_at']
-        read_only_fields = ['id', 'url', 'created_at']
-    
+        fields = ["id", "image", "url", "position", "created_at"]
+        read_only_fields = ["id", "url", "created_at"]
+
     def get_url(self, obj):
         return obj.get_url()
 
@@ -32,17 +32,27 @@ class ItemPhotoSerializer(serializers.ModelSerializer):
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'notification_type', 'reference_id', 'message', 'is_read', 'created_at']
+        fields = [
+            "id",
+            "notification_type",
+            "reference_id",
+            "message",
+            "is_read",
+            "created_at",
+        ]
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ["photo_url",'City','Bio','notifications_enabled'] 
+        fields = ["photo_url", "City", "Bio", "notifications_enabled"]
 
-class UserCreateSerializer(serializers.ModelSerializer): 
+
+class UserCreateSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=User.objects.all())]
-    )  
+    )
+
     class Meta:
         model = User
         fields = ["id", "first_name", "last_name", "email", "password"]
@@ -50,6 +60,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
             "password": {"write_only": True},
             "email": {"required": True},
         }
+
     def create(self, validated_data):
         email = validated_data.get("email")
         validated_data["username"] = email
@@ -57,8 +68,10 @@ class UserCreateSerializer(serializers.ModelSerializer):
         UserProfile.objects.create(user=user)
         return user
 
+
 class UserSerializer(serializers.ModelSerializer):
-    profile = UserProfileSerializer(source='userprofile', required=False) 
+    profile = UserProfileSerializer(source="userprofile", required=False)
+
     class Meta:
         model = User
         fields = ["id", "first_name", "last_name", "email", "password", "profile"]
@@ -68,86 +81,86 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def update(self, instance, validated_data):
-        profile_data = validated_data.pop('userprofile', {})
-        password = validated_data.pop('password', None)
-        
-        if 'email' in validated_data:
-            instance.username = validated_data['email']
+        profile_data = validated_data.pop("userprofile", {})
+        password = validated_data.pop("password", None)
+
+        if "email" in validated_data:
+            instance.username = validated_data["email"]
         if password:
             instance.set_password(password)
 
-        user = super().update(instance, validated_data) 
+        user = super().update(instance, validated_data)
         if profile_data:
-            profile_instance = user.userprofile 
-            
+            profile_instance = user.userprofile
+
             for attr, value in profile_data.items():
                 setattr(profile_instance, attr, value)
-            
+
             profile_instance.save()
-        
+
             if profile_instance.notifications_enabled:
                 Notification.objects.create(
                     user=user,
-                    notification_type='profile',
-                    message='Seu perfil e dados básicos foram atualizados!'
+                    notification_type="profile",
+                    message="Seu perfil e dados básicos foram atualizados!",
                 )
         return user
-    
+
+
 class ItemSerializer(serializers.ModelSerializer):
-    category_name = serializers.CharField(source='category.name', read_only=True)
+    category_name = serializers.CharField(source="category.name", read_only=True)
     city = CitySerializer(read_only=True)
     city_id = serializers.UUIDField(write_only=True, required=False, allow_null=True)
     photos = serializers.SerializerMethodField()
-    images = serializers.SerializerMethodField()  # Para compatibilidade com o frontend
-    type = serializers.CharField(write_only=True, required=False, default='Trade')  # Sell, Trade, ou Donation
+    images = serializers.SerializerMethodField()
+    type = serializers.CharField(write_only=True, required=False, default="Trade")
     uploaded_photos = serializers.ListField(
-        child=serializers.ImageField(),
-        write_only=True,
-        required=False
+        child=serializers.ImageField(), write_only=True, required=False
     )
-    
+
     class Meta:
         model = Item
-        fields = ['id', 'title', 'description', 'category', 'category_name', 
-                 'city', 'city_id', 'status', 'listing_state', 'created_at', 
-                 'updated_at', 'photos', 'images', 'type', 'uploaded_photos']
-        read_only_fields = ['user', 'id', 'created_at', 'updated_at', 'city']
+        fields = [
+            "id",
+            "title",
+            "description",
+            "category",
+            "category_name",
+            "city",
+            "city_id",
+            "status",
+            "listing_state",
+            "created_at",
+            "updated_at",
+            "photos",
+            "images",
+            "type",
+            "uploaded_photos",
+        ]
+        read_only_fields = ["user", "id", "created_at", "updated_at", "city"]
 
     def get_photos(self, obj):
-        return [photo.get_url() for photo in obj.photos.all().order_by('position')]
-        
+        return [photo.get_url() for photo in obj.photos.all().order_by("position")]
+
     def get_images(self, obj):
-        # Para manter compatibilidade com o frontend que espera 'images'
         return self.get_photos(obj)
 
     def create(self, validated_data):
-        # Remover campos que não pertencem ao modelo Item
-        uploaded_photos = validated_data.pop('uploaded_photos', [])
-        validated_data.pop('type', None)  # Remove 'type' pois não está no modelo
-        city_id = validated_data.pop('city_id', None)
-        
-        # Se city_id foi fornecido, buscar o objeto City
+        uploaded_photos = validated_data.pop("uploaded_photos", [])
+        validated_data.pop("type", None)
+        city_id = validated_data.pop("city_id", None)
+
         if city_id:
             try:
-                from .models import City
-                validated_data['city'] = City.objects.get(id=city_id)
+                validated_data["city"] = City.objects.get(id=city_id)
             except City.DoesNotExist:
-                validated_data['city'] = None
+                validated_data["city"] = None
         else:
-            # Se não tem city_id, remover city do validated_data (pode vir como string do frontend)
-            validated_data.pop('city', None)
-        
+            validated_data.pop("city", None)
+
         item = Item.objects.create(**validated_data)
-        
-        # Criar ItemPhoto para cada imagem enviada
+
         for index, photo in enumerate(uploaded_photos, start=1):
-            ItemPhoto.objects.create(
-                item=item,
-                image=photo,
-                position=index
-            )
-        
+            ItemPhoto.objects.create(item=item, image=photo, position=index)
+
         return item
-
-
-
