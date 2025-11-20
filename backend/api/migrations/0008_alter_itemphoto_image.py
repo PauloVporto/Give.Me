5fe587222c0b9702385c622911ad947a193b33ca
🐄1️⃣ <161668608+KBritoR@users.noreply.github.com>
2025-11-19 23:51:00 -0300

    Update 0008_alter_itemphoto_image.py

--- backend/api/migrations/0008_alter_itemphoto_image.py
+++ backend/api/migrations/0008_alter_itemphoto_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0007_alter_favorite_unique_together_alter_favorite_table'),
+        ('api', '0007_item_price'),
     ]
 
     operations = [
